1341c3c5f82dc01702926f994edf82b7ad1cdfaa
masahitojp <randomstep@gmail.com>
2020-07-29 23:07:52 +0900

    add 1st implements

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # bqdatamapper4k
 It's a glue code in BigQuery for Kotlin users.
+
+## Getting Started
+
+### Prerequisites
+
+Install OpenJDK v8 later
+
+### Installing
+
+TBD
+
+### How to use
+
+```kotlin
+data class Foo(val aaa:Long)
+
+val row: TableRow = TableRow().set("aaa", 2)
+val results: Foo = row.toDataClass<Foo>()
+assertEquals(results, Foo(2))
+
+```
+
+## Running the tests
+
+```
+./gradlew test
+```
+
+## Authors
+
+masahitojp
+
+## License
+
+Apache License, Version 2.0
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,6 @@
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
-
-    // Apply the application plugin to add support for building a CLI application.
-    application
 }
 
 repositories {
@@ -24,15 +21,12 @@ dependencies {
 
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.cloud:google-cloud-bigquery:1.116.3")
+    implementation("com.google.code.gson:gson:2.8.6")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-}
-
-application {
-    // Define the main class for the application.
-    mainClassName = "com.github.masahitojp.bqdatamapper4k.AppKt"
 }
