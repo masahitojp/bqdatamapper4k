d0866043da629c9d6454ea4bccec2e37ee4ec2dd
masahitojp <randomstep@gmail.com>
2020-11-23 12:24:55 +0900

    chore: update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.4.0"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
     id("org.jetbrains.dokka") version "0.10.1"
     `maven-publish`
 }
@@ -25,8 +25,8 @@ repositories {
 dependencies {
 
     // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.0")
-    implementation("com.google.cloud:google-cloud-bigquery:1.116.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
+    implementation("com.google.cloud:google-cloud-bigquery:1.125.0")
     implementation("com.google.code.gson:gson:2.8.6")
 
     // Use the Kotlin test library.
